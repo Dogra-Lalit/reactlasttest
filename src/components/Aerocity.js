598b95e74831db6ed415130a77132d9072f11d7e
Dogra-Lalit <124022630+Dogra-Lalit@users.noreply.github.com>
2023-09-19 16:42:42 +0530

    working on aerocity section

--- src/components/Aerocity.js
+++ src/components/Aerocity.js
@@ -1,8 +1,46 @@
 import React from "react";
+import AerocityUnion from "../images/Aerocity-union.png";
+import AerocityGroup from "../images/Aerocity-Group.png";
+import AerocityGroup1 from "../images/Aerocity-Group (1).png";
+import star from "../images/star.png";
+import sq from "../images/sq.png";
+import desk from "../images/desk.png";
 import Demandbook from './Demandbook'
 import Testimonials from './Testimonials'
 import Lets from './Lets'
+import UpcomingfindSpace from "./UpcomingfindSpace";
 const Aerocity = () => {
+  const itemsData = [
+    {
+      img: AerocityUnion,
+      desc: "Ground Floor and First Floor, Worldmark 1, Asset Area 11, Aerocity, Hospitality District, Indira Gandhi International Airport, NH-8, New Delhi, South Delhi, Delhi, 110037.",
+    },
+    {
+      img: AerocityGroup,
+      desc: "Popular restaurants, retail hubs and 5-star business hotels - within 1 km; Metro Station - 0.3 km; Indira Gandhi International Airport - 2.8 kms",
+    },
+    {
+      img: AerocityGroup1,
+      desc: "Global enterprises, public and private sector business establishments - within 0.5 km",
+    }
+  ];
+  const itemsDataSecond = [
+    {
+      img: star,
+      starrating: "5 Star",
+      rating: "100+ Ratings",
+    },
+    {
+      img: sq,
+      starrating: "5 Star",
+      rating: "100+ Ratings",
+    },
+    {
+      img: desk,
+      starrating: "5 Star",
+      rating: "100+ Ratings",
+    },
+  ];
   return (
     <>
       <div className="banner">
@@ -22,22 +60,25 @@ const Aerocity = () => {
               <div className="col-md-7">
                  <h2>Ofis Square in Aerocity</h2>
                  <ul>
-                    <li><span><img src="images/Aerocity-union.png" height="20px" width="20px"/></span>Ground Floor and First Floor, Worldmark 1, Asset Area 11, Aerocity, Hospitality District, Indira Gandhi International Airport, NH-8, New Delhi, South Delhi, Delhi, 110037</li>
-                    <li><span><img src="images/Aerocity-Group.png" height="20px" width="20px"/></span>Popular restaurants, retail hubs and 5-star business hotels - within 1 km; Metro Station - 0.3 km; Indira Gandhi International Airport - 2.8 kms</li>
-                    <li><span><img src="images/Aerocity-Group (1).png" height="20px" width="20px"/></span>Global enterprises, public and private sector business establishments - within 0.5 km</li>
+                   {itemsData.map((item, index) => (
+                     <li key={index}><span><img src={item.img} height="20px" width="20px"/></span>{item.desc}</li>
+                   ))}
                 </ul>             
               </div>
               <div className="col-md-5">
-                <div className="rigth-box"> <ul>
-                    <li><img src="images/star.png" height="25" width="25"/><h5>5 Star</h5><p>100+ Ratings</p></li>
-                    <li><img src="images/sq.png" height="25" width="25"/><h5>5 Star</h5><p>100+ Ratings</p></li>
-                    <li><img src="images/desk.png" height="25" width="25"/><h5>5 Star</h5><p>100+ Ratings</p></li>
-                   </ul></div>
+                <div className="rigth-box"> 
+                  <ul>
+                  {itemsDataSecond.map((item, index) => (
+                    <li key={index}><img src={item.img} height="25" width="25"/><h5>{item.starrating}</h5><p>{item.rating}</p></li>
+                  ))}
+                  </ul>
+                </div>
               </div>
            </div>
         </div>
      </section>
      <Demandbook/>
+     <UpcomingfindSpace/>
      <Testimonials/>
      <Lets/>
     </>
